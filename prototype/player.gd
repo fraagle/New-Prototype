398e72cd155ac61eeb971817a7ef87bb116015a7
Herman Gohn <gohnherman@gmail.com>
2025-03-27 20:48:50 +0100

    add function to calculate item-vector relative to player

--- prototype/player.gd
+++ prototype/player.gd
@@ -11,6 +11,9 @@ class_name Player
 @onready var wall_jump_timer: Timer = $WallJumpTimer
 @onready var wall_slide_timer: Timer = $WallSlideTimer
 
+# The item the player is currently targeting/handling
+var item_target: Item
+
 ## variables
 var dink_target: Dink
 var can_execute_dink: bool = false
@@ -247,13 +250,14 @@ func perform_actions(input_axis):
 	handle_dink(input_axis)
 	handle_grapple(input_axis)
 
-
+func item_vector(it: Item) -> Vector2:
+	return (it.global_position - global_position).normalized()
 
 ## Grapple
 func handle_grapple(input_axis):
 	if grapple_target != null: # and not has_executed_grapple:
 		# Updated with process!
-		grapple_direction = (grapple_target.global_position - global_position).normalized()
+		grapple_direction = item_vector(grapple_target)
 		
 		var player_detector = grapple_target.get_node("PlayerDetector/CollisionShape2D")
 		var radius = player_detector.shape.radius
@@ -283,7 +287,7 @@ func handle_dink(input_axis):
 	
 	if dink_target != null and not has_executed_dink:
 		# Updated with process!
-		dink_direction = (dink_target.global_position - global_position).normalized()
+		dink_direction = item_vector(dink_target)
 		
 		if Input.is_action_just_pressed('dink') and not is_on_floor() and not is_on_wall():
 			dink_input_axis = input_axis
